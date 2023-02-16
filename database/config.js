45666d973b9d38d408ff91386ad458aaad3d0804
Angel <ang036>
2023-02-16 01:24:08 -0400

    [IMP] INiciando sercidor con node

--- database/config.js
+++ database/config.js
@@ -1,10 +1,43 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./database/myapp', (err)=>{
+var db = new sqlite3.Database(':memory:', (err)=>{
     if(err){
         console.log(err);
     }else{
-        console.log("Conexion exitosa")
+        db.run(`CREATE TABLE user (
+            name text, 
+            surname text, 
+            email text, 
+            password text
+            )`,
+        (err) => {
+            if (err) {
+                // Table already created
+                console.log(err)
+            }else{
+                // Table just created, creating some rows
+                console.log("Base de datos creada")
+            }
+        });
+        
+        db.run(`CREATE TABLE contact (
+            name text, 
+            comment text, 
+            email text, 
+            ip text,
+            date text
+            )`,
+        (err) => {
+            if (err) {
+                // Table already created
+                console.log(err)
+            }else{
+                // Table just created, creating some rows
+                console.log("Base de datos creada")
+
+            }
+        });  
     }
 });
+
 
 module.exports = db;
